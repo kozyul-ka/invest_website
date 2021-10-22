5978888de5cdad912f714abb3d9a24d3829c726d
kozyul-ka <93002864+kozyul-ka@users.noreply.github.com>
2021-10-22 20:27:57 +0300

    goals page

---
+++ js/indexHandler.js
@@ -0,0 +1,37 @@
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    console.log(evt.currentTarget.className);
+}
+
+function openTab1(cityName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+}
+
+let but = document.getElementById('КнопкаЦели');
+openTab1('Цели');
+but.classList.add("active");
